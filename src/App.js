240f3a668bdaf5136c42d5a60464f902213f3175
moonishteapot <cole.d.vick@gmail.com>
2018-10-19 12:09:36 -0400

    auth

--- src/App.js
+++ src/App.js
@@ -9,19 +9,37 @@ import Unsplash, { toJson } from 'unsplash-js';
 const unsplash = new Unsplash({
   applicationId: "156ae7d2f43eaa131de11166c97f05b8b1b54d066f11279a3358ed8f8ff0e657",
   secret: "a17246ffd05658d87fe33a64db8bee9942cb137ac0524427aef75406d0653190",
-  callback: `${window.location.origin}`
+  callback: "https://snapapp-app.herokuapp.com/"
 });
 
-const authURL = unsplash.auth.getAuthenticationUrl([
-  "public",
-  "read_user",
-  "write_user",
-  "read_photos",
-  "write_photos"
-]);
 
+const login = () => {
+  const authenticationUrl = unsplash.auth.getAuthenticationUrl([
+    'public',
+    'read_user',
+    'write_likes',
+  ]);
 
-window.location.assign(authURL);
+  window.location.assign(authenticationUrl);
+};
+
+const authenticateFromCode = ({ code }) => {
+  unsplash.auth
+    .userAuthentication(code)
+    .then(toJson)
+    .then(json => {
+      // Set it in localstorage, so that we can use it next time
+      unsplash.auth.setBearerToken(json.access_token);
+    });
+};
+
+login();
+
+unsplash.photos.listPhotos(1, 10)
+    .then(toJson)
+    .then(json => {
+      console.log(json);
+    });
 
 class App extends Component {
 
@@ -36,11 +54,6 @@ class App extends Component {
         </header>
 
         <div class="slideshow-container">
-
-          
-
-          
-
           
 
           <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
