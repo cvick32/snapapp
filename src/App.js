a380541654c6a6845decc753cc25d628989702d3
moonishteapot <cole.d.vick@gmail.com>
2018-10-20 16:42:33 -0400

    searching functionality and grid layout

--- src/App.js
+++ src/App.js
@@ -2,66 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const appId = "156ae7d2f43eaa131de11166c97f05b8b1b54d066f11279a3358ed8f8ff0e657";
+const endPoint = "https://api.unsplash.com/photos/random?count=25&client_id=" + appId;
 
-// ES Modules syntax
-import Unsplash, { toJson } from 'unsplash-js';
+export default class App extends Component {
 
-const unsplash = new Unsplash({
-  applicationId: "156ae7d2f43eaa131de11166c97f05b8b1b54d066f11279a3358ed8f8ff0e657",
-  secret: "a17246ffd05658d87fe33a64db8bee9942cb137ac0524427aef75406d0653190",
-  callback: "https://snapapp-app.herokuapp.com/"
-});
+  state = {
+      images : []
+  };
 
+  query = '';
+  trackQuery = this.trackQuery.bind(this);
+  search = this.search.bind(this);
+   
 
-const login = () => {
-  const authenticationUrl = unsplash.auth.getAuthenticationUrl([
-    'public',
-    'read_user',
-    'write_likes',
-  ]);
+  trackQuery(ev) {
+    this.query = ev.target.value;
+  }
 
-  window.location.assign(authenticationUrl);
-};
+  search() {
+    fetch(`${endPoint}&query=${this.query}`)
+      .then(response => {
+        return response.json()
+      }).then(responseJson => {
+        this.setState({
+          images : responseJson
+        })
+      })
+  }
 
-const authenticateFromCode = ({ code }) => {
-  unsplash.auth
-    .userAuthentication(code)
-    .then(toJson)
-    .then(json => {
-      // Set it in localstorage, so that we can use it next time
-      unsplash.auth.setBearerToken(json.access_token);
-    });
-};
+  displayImages() {
+    var keyVal = 0;
+    return this.state.images.map(image => {
+      return <img src={image.urls.thumb} />
+    })
+  }
 
-login();
-
-unsplash.photos.listPhotos(1, 10)
-    .then(toJson)
-    .then(json => {
-      console.log(json);
-    });
-
-class App extends Component {
-
-  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        <h1>  </h1>
-          <img src={logo} className="App-logo" alt="logo" />
-  
-        </header>
-
-        <div class="slideshow-container">
-          
-
-          <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-          <a class="next" onclick="plusSlides(1)">&#10095;</a>
+      <div className="App">     
+        <input type="text" onChange={this.trackQuery} /><br />
+        <button onClick={this.search}>Search</button>
+        <div className="container">
+          {this.displayImages()}  
         </div>
       </div>
     );
   }
 }
 
-export default App;
+
